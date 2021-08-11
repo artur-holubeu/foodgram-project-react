c65728cdb76b538b39b7436bb2f43e202bc938e6
Artur Holubeu <artur-holubeu@yandex.by>
2021-08-11 12:03:55 +0300

    some try

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -163,7 +163,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
     'AUTH_HEADER_TYPES': ('Token',),
 }
 
